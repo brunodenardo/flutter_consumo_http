2bc48ece6651227901c59da501179cb19c3f02aa
brunodenardo <brunoden.bd@gmail.com>
2024-09-12 10:16:32 -0300

    Finalizando

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -38,6 +38,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Future<void> fetchData() async {
     final response =
         await http.get(Uri.parse("https://jsonplaceholder.typicode.com/users"));
+    print(response.body);
     if (response.statusCode == 200) {
       final jsonResponse = jsonDecode(response.body);
       setState(() {
@@ -46,6 +47,12 @@ class _MyHomePageState extends State<MyHomePage> {
     } else {
       print('Request failed with status: ${response.statusCode}.');
     }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
   }
 
   @override
@@ -62,9 +69,87 @@ class _MyHomePageState extends State<MyHomePage> {
           return Container(
             margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
             decoration: BoxDecoration(
-                color: Colors.blue, borderRadius: BorderRadius.circular(10)),
+                color: (index % 2 == 0) ? Colors.blue : Colors.green,
+                borderRadius: BorderRadius.circular(10)),
             child: ListTile(
-              title: Text('Item ${index}'),
+              title: Text('${listaUsuarios[index]["name"]}',
+                  style:
+                      TextStyle(fontSize: 24.0, fontWeight: FontWeight.w500)),
+              subtitle: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Divider(
+                    color: Colors.black,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${listaUsuarios[index]["username"]}',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          Text('${listaUsuarios[index]["email"]}'),
+                          Text('${listaUsuarios[index]["phone"]}'),
+                          Text('${listaUsuarios[index]["phone"]}'),
+                          Text('${listaUsuarios[index]["website"]}'),
+                        ],
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(8),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Empresa",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 18.0)),
+                            Text('${listaUsuarios[index]["company"]["name"]}'),
+                            Text(
+                                '${listaUsuarios[index]["company"]["catchPhrase"]}'),
+                            Text('${listaUsuarios[index]["company"]["bs"]}')
+                          ],
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.all(8),
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: const Color.fromARGB(
+                                255, 0, 0, 0), // Cor do contorno
+                            width: 1, // Largura do contorno
+                          ),
+                          borderRadius: BorderRadius.circular(
+                              10), // Arredondamento dos cantos
+                        ),
+                        child: Column(
+                          children: [
+                            const Text(
+                              "Endereço",
+                              style: TextStyle(
+                                  fontSize: 15, fontWeight: FontWeight.w500),
+                            ),
+                            Text(
+                                '${listaUsuarios[index]["address"]["street"]}'),
+                            Text('${listaUsuarios[index]["address"]["suite"]}'),
+                            Text('${listaUsuarios[index]["address"]["city"]}'),
+                            Text(
+                                '${listaUsuarios[index]["address"]["zipcode"]}'),
+                            Text(
+                                'Latitude: ${listaUsuarios[index]["address"]["geo"]["lat"]}'),
+                            Text(
+                                'Longitude: ${listaUsuarios[index]["address"]["geo"]["lng"]}'),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           );
         },
